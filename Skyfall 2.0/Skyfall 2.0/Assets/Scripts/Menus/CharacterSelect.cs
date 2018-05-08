33f6e535fdc9bd431453b55e1214f8ce4925b480
Nic1493 <nic.choy107@gmail.com>
2018-05-08 16:22:41 -0400

    start
    reuploaded master copy

---
+++ Skyfall 2.0/Skyfall 2.0/Assets/Scripts/Menus/CharacterSelect.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour
+{
+    private int playerChoice = 0;
+
+    [SerializeField]
+    private GameObject[] s_players = new GameObject[4];
+
+    [Space]
+
+    [SerializeField]
+    private GameObject selection;
+    private SpriteRenderer srSelection;
+
+    private const float minFade = 0.25f;
+    private const float maxFade = 1.00f;
+    private float fadeSpeed = 0.01f;
+
+    void Start()
+    {
+        srSelection = selection.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        GetKeyInput();
+        StartCoroutine(FadeOut(srSelection));
+    }
+
+    void GetKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && playerChoice != 0)
+        {
+            playerChoice--;
+            selection.transform.position = s_players[playerChoice].transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && playerChoice != s_players.Length)
+        {
+            playerChoice++;
+            selection.transform.position = s_players[playerChoice].transform.position;
+        }
+    }
+
+    IEnumerator FadeOut(SpriteRenderer sr)
+    {
+        for (float alpha = maxFade; alpha > minFade; alpha -= fadeSpeed)
+        {
+            Color c = sr.material.color;
+            c.a = alpha;
+            sr.material.color = c;
+            yield return null;
+        }
+        StartCoroutine(FadeIn(srSelection));
+    }
+
+    IEnumerator FadeIn(SpriteRenderer sr)
+    {
+        for (float alpha = minFade; alpha < maxFade; alpha += fadeSpeed)
+        {
+            Color c = sr.material.color;
+            c.a = alpha;
+            sr.material.color = c;
+            yield return null;
+        }
+        StartCoroutine(FadeOut(srSelection));
+    }
+}
