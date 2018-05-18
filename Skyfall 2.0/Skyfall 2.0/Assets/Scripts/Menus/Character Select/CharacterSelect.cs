dcfa1da38835c1419d8806eb3ff4e983d0bfda2b
Nic1493 <nic.choy107@gmail.com>
2018-05-18 02:52:32 -0400

    character select screen updates
    not worth the time

--- Skyfall 2.0/Skyfall 2.0/Assets/Scripts/Menus/Character Select/CharacterSelect.cs
+++ Skyfall 2.0/Skyfall 2.0/Assets/Scripts/Menus/Character Select/CharacterSelect.cs
@@ -10,32 +10,50 @@ public class CharacterSelect : MonoBehaviour
     private int menuChoice = 0;
 
     [SerializeField]
+    private GameObject leftArrow, rightArrow;
+
+    [Space]
+
+    [SerializeField]
     private GameObject[] players = new GameObject[4];
     private SpriteRenderer[] playersSprite = new SpriteRenderer[4];
+
+    [SerializeField]
+    private Transform playerCenterpoint;
 
     private float left, middle, right;
     private const float fadeSpeed = 0.1f;
     private const float moveSpeed = 0.1f;
+    private const float rotateSpeed = 3f;
+
+    private Color invisible = new Color(1, 1, 1, 0);
+    private Color visible = new Color(1, 1, 1, 1);
+
+    private bool keyInputEnabled = true;
 
     void Start()
     {
-        middle = 0f;
-        left = -moveSpeed * 20;
-        right = moveSpeed * 20;
+        middle = -4;
+        left = -6;
+        right = -2;
 
         for (int i = 0; i < players.Length; i++)
         {
             playersSprite[i] = players[i].GetComponent<SpriteRenderer>();
             if (i != menuChoice)
             {
-                playersSprite[i].material.color = new Color(1, 1, 1, 0);
+                playersSprite[i].material.color = invisible;
             }
         }
     }
 
     void Update()
     {
-        GetKeyInput();
+        ShowArrows();
+        if (keyInputEnabled)
+        {
+            GetKeyInput();
+        }
     }
 
     void GetKeyInput()
@@ -63,12 +81,41 @@ public class CharacterSelect : MonoBehaviour
         }
     }
 
+    void ShowArrows()
+    {
+        if (menuChoice == 0)
+        {
+            leftArrow.GetComponent<SpriteRenderer>().color = invisible;
+        }
+        else if (menuChoice == players.Length - 1)
+        {
+            rightArrow.GetComponent<SpriteRenderer>().color = invisible;
+        }
+        else
+        {
+            leftArrow.GetComponent<SpriteRenderer>().color = visible;
+            rightArrow.GetComponent<SpriteRenderer>().color = visible;
+        }
+    }
+
+    /*void UpdateSelection(SpriteRenderer sr, Transform t, int menuChangeAmount, )
+    {
+
+        StartCoroutine(FadeOut(sr));
+        StartCoroutine();
+        menuChoice += menuChangeAmount;
+        StartCoroutine(FadeIn(sr));
+        StartCoroutine();
+
+    }*/
+
+
     IEnumerator MoveLeft(Transform t, float from, float to)
     {
         for (float x = from; x >= to; x -= moveSpeed)
         {
             x = Mathf.Round(x * 10) / 10;
-            t.position = new Vector3 (x, t.position.y, t.position.z);
+            t.position = new Vector3(x, t.position.y, t.position.z);
             yield return null;
         }
     }
@@ -83,12 +130,28 @@ public class CharacterSelect : MonoBehaviour
         }
     }
 
+    //I CAN'T GET IT TO WORK AAAAAAA
+    /*IEnumerator Fade(SpriteRenderer sr, float start, float end, bool direction)
+    {
+        for (float i = start; i <= end; i += fadeSpeed)
+        {
+            //float j = direction ? i : (end - fadeSpeed) - i;
+            Color c = sr.material.color;
+            i = Mathf.Round(i * (1 / fadeSpeed)) / (1 / fadeSpeed);
+            c.a = i;
+            sr.material.color = c;
+            Debug.Log(i);
+            yield return null;
+        }
+    }*/
+
     IEnumerator FadeOut(SpriteRenderer sr)
     {
+        keyInputEnabled = false;
         for (float alpha = 1f; alpha >= 0f; alpha -= fadeSpeed)
         {
             Color c = sr.material.color;
-            alpha = Mathf.Round(alpha * 10) / 10;       
+            alpha = Mathf.Round(alpha * (1 / fadeSpeed)) / (1 / fadeSpeed);
             c.a = alpha;
             sr.material.color = c;
             yield return null;
@@ -100,10 +163,11 @@ public class CharacterSelect : MonoBehaviour
         for (float alpha = 0f; alpha <= 1f; alpha += fadeSpeed)
         {
             Color c = sr.material.color;
-            alpha = Mathf.Round(alpha * 10) / 10;
+            alpha = Mathf.Round(alpha * (1 / fadeSpeed)) / (1 / fadeSpeed);
             c.a = alpha;
             sr.material.color = c;
             yield return null;
         }
+        keyInputEnabled = true;
     }
 }
