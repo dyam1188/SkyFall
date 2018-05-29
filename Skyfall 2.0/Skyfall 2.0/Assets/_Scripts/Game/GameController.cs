e58039ff2610d846bcf3809f3c00d217af6b5c1f
Nic1493 <nic.choy107@gmail.com>
2018-05-29 15:20:27 -0400

    script updates
    preparing for AI creation

---
+++ Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Game/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//controls game's main functions
+//attached to Game -> Script Holder - Game
+public class GameController : MonoBehaviour
+{
+    public Transform spawnTransform;
+
+    private bool isPaused;
+
+    void Awake()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.transform.position = spawnTransform.position;
+        player.transform.localScale = new Vector3(0.5f, 0.5f);
+    }
+
+    void Start()
+    {
+        //todo: wangs
+    }
+
+    void Update()
+    {
+        if (!isPaused)
+        {
+            Run(); 
+        }
+    }
+
+    void Run()
+    {
+        //game stuffs
+    }
+}
