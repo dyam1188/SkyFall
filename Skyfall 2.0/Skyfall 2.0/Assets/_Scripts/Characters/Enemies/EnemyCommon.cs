1e9c5d9ad7aba10761ca49c2bfb713c0d4bc943a
dyam1188 <066255860a@gmail.com>
2018-05-31 16:10:25 -0400

    Enemy Scriptable Object

---
+++ Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Characters/Enemies/EnemyCommon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Enemy Common", menuName = "Enemy Common")]
+public class EnemyCommon : ScriptableObject {
+    public Sprite enemySprite;
+    public Bullet enemyBullet;
+
+    [Space]
+
+    public int health;
+    public int attack;
+    public int defense;
+    public int moveSpeed;
+    public int shotDensity;
+}
---
+++ Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Characters/Enemies/EnemyController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public EnemyCommon enemy;
+    public Bullet enemyBullet;
+    public GameObject bulletGameObject;
+
+    [SerializeField]
+    private Transform bulletSpawn;
+
+    private int health;
+    private int attack;
+    private int defense;
+    private int moveSpeed;
+    private int shotDensity;
+
+    private bool shootEnabled;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        GetComponent<SpriteRenderer>().sprite = enemy.enemySprite;
+        health = enemy.health;
+        attack = enemy.attack;
+        defense = enemy.defense;
+
+        moveSpeed = enemy.moveSpeed;
+        shotDensity = enemy.shotDensity;
+    }
+
+    void Start()
+    {
+        bulletGameObject.GetComponent<BulletController>().bullet = enemyBullet;
+        shootEnabled = true;
+    }
+
+    void Update()
+    {
+        if (shootEnabled)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        ShootBullet();
+        shootEnabled = false;
+        StartCoroutine(Delay(1f / shotDensity));
+    }
+
+    IEnumerator Delay(float t)
+    {
+        yield return new WaitForSeconds(t);
+        shootEnabled = true;
+    }
+
+    void ShootBullet()
+    {
+        Instantiate(bulletGameObject, bulletSpawn.position, bulletSpawn.rotation);
+    }
+}
