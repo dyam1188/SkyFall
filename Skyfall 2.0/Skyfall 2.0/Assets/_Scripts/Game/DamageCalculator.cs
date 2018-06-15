9369f3bbd1c3b22238522e66659a2354d34ba294
dyam1188 <066255860a@gmail.com>
2018-06-15 01:03:30 -0400

    damage formula

--- Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Game/DamageCalculator.cs
+++ Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Game/DamageCalculator.cs
@@ -8,10 +8,12 @@ public class DamageCalculator : MonoBehaviour
     //how much to randomize the damage by
     //we don't want the damage amount to be the exact same every single time
     float variance = 0.05f;
+    float atkVariance = 0.04f;
+    float defVariance = 0.02f;
 
     public float CalculateDamage(float attack, float defense)
     {
-        float damage = (attack - defense);
+        float damage = (attack * (40 * Random.Range(0, atkVariance))) - (defense * (20 * Random.Range(0, defVariance)));
 
         //add variance, clamp it down to [0, infinity], and round it back to a whole number
         damage = Mathf.Clamp(Mathf.Round(damage * (1 + Random.Range(-variance, variance))), 0, Mathf.Infinity);
