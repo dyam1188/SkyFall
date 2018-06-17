9125270452b940d81cc1d9062668ddc07caf93b0
dyam1188 <066255860a@gmail.com>
2018-06-17 01:58:23 -0400

    gold text

---
+++ Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Game/UIScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public int gold;
+
+    [SerializeField]
+    Text GoldText;
+
+    public Transform UICanvas;
+    public Slider HealthSlider;
+    public Player player;
+
+    // Use this for initialization
+    void Start () {
+        gold = 000;
+
+        //HealthSlider.value = currentHealth / maxHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GoldText.text = gold.ToString();
+    }
+}
