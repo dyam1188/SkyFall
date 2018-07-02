fd6e20cece338a41252a766a47e15cbb0a5c0ed0
dyam1188 <066255860a@gmail.com>
2018-07-02 19:52:31 -0400

    Boss Prefabs

---
+++ Skyfall 2.0/Skyfall 2.0/Assets/_Scripts/Characters/Enemies/EnemySaji.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySaji : Enemy {
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
